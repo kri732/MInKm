f2e0c621f742ef5f1559693b8214ea6b5f7c0b47
kri732 <kirevd9@gmail.com>
2024-10-07 21:00:11 +0300

    первый коммит

---
+++ mInKm.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    cout << "Ввеите метры: ";
+    float meatr;
+    cin >> meatr;
+    cout << "Ваши километры " << meatr / 1000;
+
+}
+
+
